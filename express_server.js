3506047873ea87a648bd08fb53902af7bd547715
James <jameshuang337@gmail.com>
2021-12-07 17:20:49 -0800

    added post route, body parser, and random string generator function

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// Installing a piece of middleware: body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // This tells the Express app to use EJS as its templating engine
 app.set("view engine", "ejs")
 
@@ -23,6 +27,15 @@ app.get("/urls", (req, res) => {
     res.render('urls_index', templateVars)
 });
 
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
@@ -39,3 +52,11 @@ app.listen(PORT, () => {
 })
 
 
+function generateRandomString() { 
+    let randomString = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 6; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return randomString;
+}
